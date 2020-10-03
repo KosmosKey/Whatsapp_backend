e6c039c73406674c99d23314f2305b21e15f0e69
KosmosKey <khamidovmaga2002@gmail.com>
2020-10-03 18:42:10 +0200

    Making mongoose

--- whatsapp-back/server.js
+++ whatsapp-back/server.js
@@ -1,7 +1,9 @@
-const express = require("express");
+import express from "express";
+import mongoose from "mongoose";
+import Messages from "./dbMessages";
+
 const app = express();
 
-const mongoose = require("mongoose");
 const port = process.env.PORT || 9000;
 
 const connection_url =
@@ -14,5 +16,17 @@ mongoose.connect(connection_url, {
 });
 
 app.get("/", (req, res) => res.status(200).send("Hello world"));
+
+app.post("/api/v1/messages/new", (req, res) => {
+  const dbMessage = req.body;
+
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(`new message is created: ${data}`);
+    }
+  });
+});
 
 app.listen(port, () => console.log(`Running on port:${port}`));
